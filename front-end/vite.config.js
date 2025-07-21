3d127eb6406a415486bba647791261fb525c6ab3
ariel <arielhodaya@gmail.com>
2025-07-21 16:11:39 +0300

    new

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // חשוב מאוד
+  base: "/", // חשוב מאוד
   build: {
     outDir: "dist",
     rollupOptions: {
